bc83c4c11d29bd4f94edb49dd78d69e354c7a80b
Trenton Telge <5915803+trenton-telge@users.noreply.github.com>
2018-10-12 15:09:54 -0500

    Added TODOs

--- src/edu/uh/plainsight/PlainSight.java
+++ src/edu/uh/plainsight/PlainSight.java
@@ -3,5 +3,6 @@ package edu.uh.plainsight;
 public class PlainSight {
     public static void main(String[] args){
         //TODO run
+        //TODO make a new StartingFrame
     }
 }
---
+++ src/edu/uh/plainsight/gui/StartingFrame.java
@@ -0,0 +1,18 @@
+package edu.uh.plainsight.gui;
+
+import javax.swing.*;
+
+public class StartingFrame extends JFrame {
+    //TODO implement a JFrame with options to encode or decode
+        //TODO IF ENCODE
+    //TODO make a JFileChooser to choose starting image
+    //TODO make a JFileChooser to choose file to encrypt in image
+    //TODO start new thread for encryption
+    //TODO make progress bar frame
+    //TODO make output frame on thread close
+        //TODO IF DECODE
+    //TODO make a frame with buttons to choose starting image, encrypted image, and destination
+    //TODO make new thread for decryption
+    //TODO make progress bar frame
+    //TODO make output frame on thread close
+}
