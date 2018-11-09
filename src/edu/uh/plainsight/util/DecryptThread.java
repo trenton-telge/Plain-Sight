69643a921d051cda4fe564632308beae18f2953d
Trenton Telge <5915803+trenton-telge@users.noreply.github.com>
2018-11-09 13:53:18 -0600

    Added file extension decryption

--- src/edu/uh/plainsight/util/DecryptThread.java
+++ src/edu/uh/plainsight/util/DecryptThread.java
@@ -55,21 +55,24 @@ public class DecryptThread extends Thread {
             System.out.println("Desaturated.");
             System.out.println("Reading metadata...");
             long fileSize = 0;
-            fileSize += (encryptedRGB[height-1][width-4].getRed() - baseRGB[height-1][width-5].getRed()) * Math.pow(10, 11);
-            fileSize += (encryptedRGB[height-1][width-4].getGreen() - baseRGB[height-1][width-5].getGreen()) * Math.pow(10, 10);
-            fileSize += (encryptedRGB[height-1][width-4].getBlue() - baseRGB[height-1][width-5].getBlue()) * Math.pow(10, 9);
-            fileSize += (encryptedRGB[height-1][width-3].getRed() - baseRGB[height-1][width-5].getRed()) * Math.pow(10, 8);
-            fileSize += (encryptedRGB[height-1][width-3].getGreen() - baseRGB[height-1][width-5].getGreen()) * Math.pow(10, 7);
-            fileSize += (encryptedRGB[height-1][width-3].getBlue() - baseRGB[height-1][width-5].getBlue()) * Math.pow(10, 6);
-            fileSize += (encryptedRGB[height-1][width-2].getRed() - baseRGB[height-1][width-5].getRed()) * Math.pow(10, 5);
-            fileSize += (encryptedRGB[height-1][width-2].getGreen() - baseRGB[height-1][width-5].getGreen()) * Math.pow(10, 4);
-            fileSize += (encryptedRGB[height-1][width-2].getBlue() - baseRGB[height-1][width-5].getBlue()) * Math.pow(10, 3);
-            fileSize += (encryptedRGB[height-1][width-1].getRed() - baseRGB[height-1][width-5].getRed()) * Math.pow(10, 2);
-            fileSize += (encryptedRGB[height-1][width-1].getGreen() - baseRGB[height-1][width-5].getGreen()) * Math.pow(10, 1);
-            fileSize += (encryptedRGB[height-1][width-1].getBlue() - baseRGB[height-1][width-5].getBlue()) * Math.pow(10, 0);
+            fileSize += (encryptedRGB[height-1][width-4].getRed() - baseRGB[height-1][width-4].getRed()) * Math.pow(10, 11);
+            fileSize += (encryptedRGB[height-1][width-4].getGreen() - baseRGB[height-1][width-4].getGreen()) * Math.pow(10, 10);
+            fileSize += (encryptedRGB[height-1][width-4].getBlue() - baseRGB[height-1][width-4].getBlue()) * Math.pow(10, 9);
+            fileSize += (encryptedRGB[height-1][width-3].getRed() - baseRGB[height-1][width-3].getRed()) * Math.pow(10, 8);
+            fileSize += (encryptedRGB[height-1][width-3].getGreen() - baseRGB[height-1][width-3].getGreen()) * Math.pow(10, 7);
+            fileSize += (encryptedRGB[height-1][width-3].getBlue() - baseRGB[height-1][width-3].getBlue()) * Math.pow(10, 6);
+            fileSize += (encryptedRGB[height-1][width-2].getRed() - baseRGB[height-1][width-2].getRed()) * Math.pow(10, 5);
+            fileSize += (encryptedRGB[height-1][width-2].getGreen() - baseRGB[height-1][width-2].getGreen()) * Math.pow(10, 4);
+            fileSize += (encryptedRGB[height-1][width-2].getBlue() - baseRGB[height-1][width-2].getBlue()) * Math.pow(10, 3);
+            fileSize += (encryptedRGB[height-1][width-1].getRed() - baseRGB[height-1][width-1].getRed()) * Math.pow(10, 2);
+            fileSize += (encryptedRGB[height-1][width-1].getGreen() - baseRGB[height-1][width-1].getGreen()) * Math.pow(10, 1);
+            fileSize += (encryptedRGB[height-1][width-1].getBlue() - baseRGB[height-1][width-1].getBlue()) * Math.pow(10, 0);
             System.out.println(" Encrypted data size is " + fileSize + " bytes.");
-            String ext = "";
-
+            String ext = ImageUtil.concatHalfBytesToASCII((encryptedRGB[height-1][width-8].getGreen() - baseRGB[height-1][width-8].getGreen()), (encryptedRGB[height-1][width-8].getBlue() - baseRGB[height-1][width-8].getBlue())) == (char)0? "" : Character.toString(ImageUtil.concatHalfBytesToASCII((encryptedRGB[height-1][width-8].getGreen() - baseRGB[height-1][width-8].getGreen()), (encryptedRGB[height-1][width-8].getBlue() - baseRGB[height-1][width-8].getBlue())));
+            ext = ext.concat(ImageUtil.concatHalfBytesToASCII((encryptedRGB[height-1][width-7].getGreen() - baseRGB[height-1][width-7].getGreen()), (encryptedRGB[height-1][width-7].getBlue() - baseRGB[height-1][width-7].getBlue())) == (char)0? "" : Character.toString(ImageUtil.concatHalfBytesToASCII((encryptedRGB[height-1][width-7].getGreen() - baseRGB[height-1][width-7].getGreen()), (encryptedRGB[height-1][width-7].getBlue() - baseRGB[height-1][width-7].getBlue()))));
+            ext = ext.concat(ImageUtil.concatHalfBytesToASCII((encryptedRGB[height-1][width-6].getGreen() - baseRGB[height-1][width-6].getGreen()), (encryptedRGB[height-1][width-6].getBlue() - baseRGB[height-1][width-6].getBlue())) == (char)0? "" : Character.toString(ImageUtil.concatHalfBytesToASCII((encryptedRGB[height-1][width-6].getGreen() - baseRGB[height-1][width-6].getGreen()), (encryptedRGB[height-1][width-6].getBlue() - baseRGB[height-1][width-6].getBlue()))));
+            ext = ext.concat(ImageUtil.concatHalfBytesToASCII((encryptedRGB[height-1][width-5].getGreen() - baseRGB[height-1][width-5].getGreen()), (encryptedRGB[height-1][width-5].getBlue() - baseRGB[height-1][width-5].getBlue())) == (char)0? "" : Character.toString(ImageUtil.concatHalfBytesToASCII((encryptedRGB[height-1][width-5].getGreen() - baseRGB[height-1][width-5].getGreen()), (encryptedRGB[height-1][width-5].getBlue() - baseRGB[height-1][width-5].getBlue()))));
+            System.out.println(" File type is " + ext.toUpperCase() + ".");
         } catch (IOException e){
             e.printStackTrace();
         }
